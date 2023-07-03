c93dc3fb9ec1a52a1f09ef2ded054e03166e9f52
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-04 02:56:48 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h9tdnqu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
