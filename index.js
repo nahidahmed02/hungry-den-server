e0cdaf677b92aeb96bab8abae676ede6125ad5df
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-02 02:57:13 +0600

    api to delete a review

--- index.js
+++ index.js
@@ -151,6 +151,14 @@ async function run() {
             res.send(result);
         })
 
+        // to delete a review
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     } finally {
 
